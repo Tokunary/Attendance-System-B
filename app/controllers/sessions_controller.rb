05021c37cca117af7b341c9b02cf79100175d12a
Tokunary <t0kufa_y@yahoo.co.jp>
2020-08-01 14:29:45 +0000

    チェックボックス追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,9 +7,10 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       redirect_to user
     else
-      flash.now[:danger] = "認証に失敗しました。"
+      flash.now[:danger] = '認証に失敗しました。'
       render :new
     end
   end
