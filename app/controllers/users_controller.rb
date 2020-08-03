67cd6f71deaea040fb9f1e7ec3dbd968e4027b71
Tokunary <t0kufa_y@yahoo.co.jp>
2020-08-03 21:09:24 +0000

    before_actionでログインユーザーか確認

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,9 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
   before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :correct_user, only:[:edit, :update]
   
   def show
-    @user = User.find(params[:id])
   end
   
   def new
@@ -21,11 +22,9 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
@@ -41,6 +40,11 @@ class UsersController < ApplicationController
     end
     
     # beforeフィルター
+    
+    # paramsハッシュからユーザーを取得
+    def set_user
+      @user = User.find(params[:id])
+    end
 
     # ログイン済みのユーザーか確認
     def logged_in_user
@@ -48,5 +52,10 @@ class UsersController < ApplicationController
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
+    end
+    
+    # アクセスしたユーザーが現在ログインしているユーザーか確認
+    def correct_user
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
